4316da150cdb6f1ae4ae571cf691e4be25284a20
Tbensalem <tbensalem@yahoo.com>
2023-11-06 15:23:03 -0500

    more boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: [
         './client/index.js'
     ],
@@ -10,15 +11,59 @@ module.exports = {
         publicPath: '/',
         filename: 'bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                            ['@babel/preset-react', { targets: 'defaults' }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        modules: [path.resolve(__dirname, 'client'), 'node_modules'],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './client.index.html'
         })
     ],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-        modules: [path.resolve(__dirname, 'client'), 'node_modules'],
+    devServer: {
+        host: 'localhost',
+        port: process.env.FRONTEND_PORT || 8080,
+        static: {
+            directory: path.join(__dirname, '/build'),
+            publicPath: '/build/bundle.js'
+        },
+        hot: true,
+        proxy: {
+            '/db/**': {
+                target: 'http://localhost:3000/',
+                secure: false,
+              },
+              '/status/**': {
+                target: 'http://localhost:3000/',
+                secure: false,
+              },
+              '/google/**': {
+                target: 'http://localhost:3000/',
+                secure: false,
+              },
+        },
+        historyApiFallback: true,
     },
-    mode: 'development',
-
 };
