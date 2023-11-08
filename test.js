8dd1062fa50eecac8e6f9cb96f9b6f3ecc04dfb9
Tbensalem <tbensalem@yahoo.com>
2023-11-08 16:17:24 -0500

    fixed react router bug

--- test.js
+++ test.js
@@ -1,27 +1,4 @@
-const twoSum = (arr, target) => {
-
-  if (arr.length === 0) return false
-
-  let leftPointer = 0;
-  let rightPointer = arr.length - 1;
-
-  const sortedArray = arr.sort((a, b) => a - b);
-  while (leftPointer < rightPointer) {
-    const sum = sortedArray[leftPointer] + sortedArray[rightPointer];
-    if (sum === target) {//
-      return true;
-    }
-    else if (sum < target) {
-      leftPointer++;
-    }
-    else if (sum > target) {
-      rightPointer--;
-    }
-  }
-
-  return false;
-
-}
+dfasdfsadfasdfas
 let result = "";
 const arr1 = [1, 4, 6, 12, 9];
 result = result.concat(twoSum(arr1, 10));
